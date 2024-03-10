95a8676233109bb8c8912c8aa3beb774245961e9
Himanshu Deulkar <97305434+HimanshuGD@users.noreply.github.com>
2024-03-10 13:13:49 +0530

    Add files via upload

---
+++ src/Components/Album/Fav./track.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+
+import bts1s from './songs/1.mp3';
+import bts2s from './songs/2.mp3';
+import bts3s from './songs/3.mp3';
+import bts4s from './songs/4.mp3';
+import bts5s from './songs/5.mp3';
+import bts6s from './songs/6.mp3';
+import bts7s from './songs/7.m4a';
+import bts8s from './songs/8.m4a';
+import bts9s from './songs/9.mp3';
+import bts10s from './songs/10.mp3';
+import bts11s from './songs/11.mp3';
+import bts12s from './songs/12.mp3';
+import bts13s from './songs/13.mp3';
+import bts14s from './songs/14.mp3';
+import bts15s from './songs/15.mp3';
+
+
+import cov1 from './covers/1.jpg';
+import cov2 from './covers/2.jpg';
+import cov3 from './covers/3.jpg';
+import cov4 from './covers/4.jpg';
+import cov5 from './covers/5.jpg';
+import cov6 from './covers/6.jpg';
+import cov7 from './covers/7.jpg';
+import cov8 from './covers/8.jpg';
+import cov9 from './covers/9.jpg';
+import cov10 from './covers/10.jpg';
+import cov11 from './covers/11.jpg';
+import cov12 from './covers/bggg.jpg';
+import cov13 from './covers/markus-spiske-9Qv774YDKbA-unsplash (1).jpg';
+
+
+export const tracks = [
+    { author: 'Charlie Puth', title: 'One Call Away', src: bts1s, thumbnail: cov1 },
+    { author: 'Chris Brown', title: 'With you', src: bts2s, thumbnail: cov2 },
+    { author: 'Adam Levine', title: 'Daylight', src: bts3s, thumbnail: cov3 },
+    { author: 'Ellie Goulding', title: 'Love me like you do', src: bts4s, thumbnail: cov4 },
+    { author: 'Eurythmics', title: 'Sweet Dreams', src: bts5s, thumbnail: cov5 },
+    { author: 'Fun', title: 'We are young', src: bts6s, thumbnail: cov6 },
+    { author: 'Imagine Dragons', title: 'Bad Liar', src: bts7s, thumbnail: cov7 },
+    { author: 'Imagine Dragons', title: 'Believer', src: bts8s, thumbnail: cov8 },
+    { author: 'Imagine Dragons', title: 'Bird', src: bts9s, thumbnail: cov9 },
+    { author: 'Imagine Dragons', title: 'Shot', src: bts10s, thumbnail: cov10 },
+    { author: 'Imagine Dragons', title: 'Whatever it takes', src: bts11s, thumbnail: cov11 },
+    { author: 'Police', title: 'Every Breath you take', src: bts12s, thumbnail: cov12 },
+    { author: 'Lil Dicky ft. Chris Brown', title: 'Freaky Friday', src: bts13s, thumbnail: cov13 },
+    { author: 'XXXTENTACION', title: 'Scared of the dark', src: bts14s, thumbnail: cov12 },
+    { author: 'Jackson Wang', title: 'Internet Money', src: bts15s, thumbnail: cov13 },
+    
+];
+
