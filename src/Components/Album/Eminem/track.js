0fbb4f0352fce61037fea4b0cf8276b8a3491c8d
Himanshu Deulkar <97305434+HimanshuGD@users.noreply.github.com>
2024-03-10 12:35:46 +0530

    Add files via upload

---
+++ src/Components/Album/Eminem/track.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+
+import bts1s from './songs/1.mp3';
+import bts2s from './songs/2.mp3';
+import bts3s from './songs/3.mp3';
+import bts4s from './songs/4.mp3';
+import bts5s from './songs/5.mp3';
+
+import cov1 from './covers/1.jpg';
+import cov2 from './covers/2.jpg';
+import cov3 from './covers/3.jpg';
+import cov4 from './covers/4.jpg';
+import cov5 from './covers/5.jpg';
+import cov6 from './covers/6.jpg';
+import cov7 from './covers/7.jpg';
+
+export const tracks = [
+    { author: 'himanshu', title: 'Fall', src: bts1s, thumbnail: cov1 },
+    { author: 'himanshu', title: 'Love the way you are', src: bts2s, thumbnail: cov2 },
+    { author: 'himanshu', title: 'Not Afraid', src: bts3s, thumbnail: cov3 },
+    { author: 'himanshu', title: 'Venom', src: bts4s, thumbnail: cov4 },
+    { author: 'himanshu', title: 'Rap God', src: bts5s, thumbnail: cov5 },
+];
+
