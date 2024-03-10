a3702c8ba66ec1278f512b9b9a249e9d693dbe55
Himanshu Deulkar <97305434+HimanshuGD@users.noreply.github.com>
2024-03-10 12:39:52 +0530

    Add files via upload

---
+++ src/Components/Album/BTS/track.js
@@ -0,0 +1,77 @@
+import React from "react";
+
+
+import bts1s from './songs/1.mp3';
+import bts2s from './songs/2.mp3';
+import bts3s from './songs/3.mp3';
+import bts4s from './songs/4.mp3';
+import bts5s from './songs/5.mp3';
+import bts6s from './songs/6.mp3';
+import bts7s from './songs/7.mp3';
+import bts8s from './songs/8.mp3';
+import bts9s from './songs/9.mp3';
+import bts10s from './songs/10.mp3';
+import bts11s from './songs/11.mp3';
+import bts12s from './songs/12.mp3';
+import bts13s from './songs/13.mp3';
+import bts14s from './songs/14.mp3';
+import bts15s from './songs/15.mp3';
+import bts16s from './songs/16.mp3';
+import bts17s from './songs/17.mp3';
+import bts18s from './songs/18.mp3';
+import bts19s from './songs/19.mp3';
+import bts20s from './songs/20.mp3';
+import bts21s from './songs/21.mp3';
+import bts22s from './songs/22.mp3';
+import bts23s from './songs/23.mp3';
+
+import cov1 from './covers/1.jpg';
+import cov2 from './covers/2.jpg';
+import cov3 from './covers/3.jpg';
+import cov4 from './covers/4.jpg';
+import cov5 from './covers/5.jpg';
+import cov6 from './covers/6.jpg';
+import cov7 from './covers/7.jpg';
+import cov8 from './covers/8.jpg';
+import cov9 from './covers/9.jpg';
+import cov10 from './covers/10.jpg';
+import cov11 from './covers/11.jpg';
+import cov12 from './covers/12.jpg';
+import cov13 from './covers/13.jpg';
+import cov14 from './covers/14.jpg';
+import cov15 from './covers/15.jpg';
+import cov16 from './covers/1.jpg';
+import cov17 from './covers/17.jpg';
+import cov18 from './covers/18.jpg';
+import cov19 from './covers/19.jpg';
+import cov20 from './covers/20.jpg';
+import cov21 from './covers/21.jpg';
+import cov22 from './covers/22.jpg';
+import cov23 from './covers/23.jpg';
+
+export const tracks = [
+    { author: 'himanshu', title: 'Magic Shop', src: bts1s, thumbnail: cov1 },
+    { author: 'himanshu', title: 'Mikrokosmos', src: bts2s, thumbnail: cov2 },
+    { author: 'himanshu', title: 'Black Swan', src: bts3s, thumbnail: cov3 },
+    { author: 'himanshu', title: 'DNA', src: bts4s, thumbnail: cov4 },
+    { author: 'himanshu', title: 'Fake Love', src: bts5s, thumbnail: cov5 },
+    { author: 'himanshu', title: 'Persona', src: bts6s, thumbnail: cov6 },
+    { author: 'himanshu', title: 'Shadow', src: bts7s, thumbnail: cov7 },
+    { author: 'himanshu', title: 'Eight ft. Suga', src: bts8s, thumbnail: cov8 },
+    { author: 'himanshu', title: '10000Hours by JB', src: bts9s, thumbnail: cov9 },
+    { author: 'himanshu', title: 'Stay Gold', src: bts10s, thumbnail: cov10 },
+    { author: 'himanshu', title: 'Idol', src: bts11s, thumbnail: cov11 },
+    { author: 'himanshu', title: 'On', src: bts12s, thumbnail: cov12 },
+    { author: 'himanshu', title: 'Dont Leave Me', src: bts13s, thumbnail: cov13 },
+    { author: 'himanshu', title: 'We Are Bulletproof', src: bts14s, thumbnail: cov14 },
+    { author: 'himanshu', title: 'Still With You', src: bts15s, thumbnail: cov15 },
+    { author: 'himanshu', title: 'Waste on me ft.Steve Aoki', src: bts16s, thumbnail: cov16 },
+    { author: 'himanshu', title: 'Euphoria', src: bts17s, thumbnail: cov17 },
+    { author: 'himanshu', title: 'Savage Love', src: bts18s, thumbnail: cov18 },
+    { author: 'himanshu', title: 'Spring', src: bts19s, thumbnail: cov19 },
+    { author: 'himanshu', title: 'Life goes on', src: bts20s, thumbnail: cov20 },
+    { author: 'himanshu', title: 'Sweet Night', src: bts21s, thumbnail: cov21 },
+    { author: 'himanshu', title: 'So what', src: bts22s, thumbnail: cov22 },
+    { author: 'himanshu', title: 'I need you', src: bts23s, thumbnail: cov23 },
+];
+
