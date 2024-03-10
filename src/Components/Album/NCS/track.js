185356064fa3d519c1f6efd274ef16233ac6e0c8
Himanshu Deulkar <97305434+HimanshuGD@users.noreply.github.com>
2024-03-10 12:28:57 +0530

    Add files via upload

---
+++ src/Components/Album/NCS/track.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+
+import bts1s from './songs/1.mp3';
+import bts2s from './songs/2.mp3';
+import bts3s from './songs/3.mp3';
+import bts4s from './songs/4.mp3';
+import bts5s from './songs/5.mp3';
+import bts6s from './songs/6.mp3';
+import bts7s from './songs/7.mp3';
+import bts8s from './songs/8.mp3';
+import bts9s from './songs/9.mp3';
+import bts10s from './songs/10.mp3';
+import bts11s from './songs/11.mp3';
+
+
+import cov1 from './covers/1.jpg';
+import cov2 from './covers/2.jpg';
+import cov3 from './covers/3.jpg';
+import cov4 from './covers/4.jpg';
+import cov5 from './covers/5.jpg';
+import cov6 from './covers/6.jpg';
+import cov7 from './covers/7.jpg';
+import cov8 from './covers/8.jpg';
+import cov9 from './covers/9.jpg';
+import cov10 from './covers/10.jpg';
+import cov11 from './covers/11.jpg';
+
+
+export const tracks = [
+    { author: 'himanshu', title: 'Magic Shop', src: bts1s, thumbnail: cov1 },
+    { author: 'himanshu', title: 'Mikrokosmos', src: bts2s, thumbnail: cov2 },
+    { author: 'himanshu', title: 'Black Swan', src: bts3s, thumbnail: cov3 },
+    { author: 'himanshu', title: 'DNA', src: bts4s, thumbnail: cov4 },
+    { author: 'himanshu', title: 'Fake Love', src: bts5s, thumbnail: cov5 },
+    { author: 'himanshu', title: 'Persona', src: bts6s, thumbnail: cov6 },
+    { author: 'himanshu', title: 'Shadow', src: bts7s, thumbnail: cov7 },
+    { author: 'himanshu', title: 'Eight ft. Suga', src: bts8s, thumbnail: cov8 },
+    { author: 'himanshu', title: '10000Hours by JB', src: bts9s, thumbnail: cov9 },
+    { author: 'himanshu', title: 'Stay Gold', src: bts10s, thumbnail: cov10 },
+    { author: 'himanshu', title: 'Idol', src: bts11s, thumbnail: cov11 },
+    
+];
+
---
+++ src/Components/Album/Taylor_S/track.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+
+import bts1s from './songs/1.mp3';
+import bts2s from './songs/2.mp3';
+import bts3s from './songs/3.mp3';
+import bts4s from './songs/4.mp3';
+import bts5s from './songs/5.mp3';
+
+
+import cov1 from './covers/1.jpg';
+import cov2 from './covers/2.jpg';
+import cov3 from './covers/3.jpg';
+import cov4 from './covers/4.jpg';
+import cov5 from './covers/5.jpg';
+import cov6 from './covers/6.jpg';
+
+
+export const tracks = [
+    { author: 'himanshu', title: 'Magic Shop', src: bts1s, thumbnail: cov1 },
+    { author: 'himanshu', title: 'Mikrokosmos', src: bts2s, thumbnail: cov2 },
+    { author: 'himanshu', title: 'Black Swan', src: bts3s, thumbnail: cov3 },
+    { author: 'himanshu', title: 'DNA', src: bts4s, thumbnail: cov4 },
+    { author: 'himanshu', title: 'Fake Love', src: bts5s, thumbnail: cov5 },
+];
+
